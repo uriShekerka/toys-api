26b4120380d285d16aa914b14befaa8c2f408600
uriShekerka <116493322+uriShekerka@users.noreply.github.com>
2022-12-11 03:00:47 +0200

    Add files via upload

--- model/toysModel.js
+++ model/toysModel.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const { string } = require("joi");
 
 const ToysSchema = new mongoose.Schema({
     name: String,
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,5 @@
 const express= require("express");
 const bcrypt = require("bcrypt");
-const {auth} = require("../middleweres/auth");
-
 const { validteUser, UserModel, validteLogin, createToken } = require("../model/userModel");
 const router = express.Router();
 
